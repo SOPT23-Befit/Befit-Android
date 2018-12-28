60404b8ea31e8450359f30fdb4caefc555530b36
송지현 <sgh280@naver.com>
2018-12-28 21:23:04 +0900

    login activity

---
+++ app/src/main/java/com/sopt/befit/activity/LogInActivity.kt
@@ -0,0 +1,26 @@
+package com.sopt.befit.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sopt.befit.R
+import kotlinx.android.synthetic.main.activity_log_in.*
+import org.jetbrains.anko.startActivity
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        btn_log_in_join.setOnClickListener(){
+            startActivity<SignUpActivity>()
+        }
+        btn_log_in_confirm.setOnClickListener(){
+
+            startActivity<MainActivity>()//
+        }
+        btn_log_in_just_look.setOnClickListener(){
+            startActivity<MainActivity>()
+        }
+    }
+}
