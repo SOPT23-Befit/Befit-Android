012bd0722f5c4ba38c92cb4330653f071fffba3f
Jeongmin <han51361@naver.com>
2018-12-31 17:30:19 +0900

    Add structure - Sign up / Find email view / Find password view & Delete Dialog Login fragment

--- app/src/main/java/com/sopt/befit/network/NetworkService.kt
+++ app/src/main/java/com/sopt/befit/network/NetworkService.kt
@@ -1,6 +1,8 @@
 package com.sopt.befit.network
 
 import com.google.gson.JsonObject
+import com.sopt.befit.data.LoginData
+import com.sopt.befit.data.UserData
 import com.sopt.befit.post.PostLoginResponse
 import com.sopt.befit.post.PostSignUpResponse
 import retrofit2.Call
@@ -14,14 +16,16 @@ interface NetworkService {
     //회원 가입
     @POST("/user")
     fun postSignUpResponse(
-            @Header("Content-Type: application/json") content_type: String,
-            @Body body: JsonObject
+            @Header("Content-Type: application/json")
+            @Body userData: UserData
     ) : Call<PostSignUpResponse>
 
 
     @POST("/login")
     fun postLoginResponse(
-            @Header("Content-Type") content_type : String,
-            @Body() body : JsonObject
+            @Header("Content-Type: application/json")
+            @Body() loginData: LoginData
     ): Call<PostLoginResponse>
+
+
 }
