a395b4d65f245de6ab5a15a76de0012ac8cdad28
weenybeenymini <alsgml9091@gmail.com>
2019-01-02 20:10:27 +0900

    why_no_run

---
+++ app/src/main/java/com/sopt/befit/activity/MySizeLookupActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.befit.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MySizeLookupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_size_lookup)
+    }
+}
---
+++ app/src/main/java/com/sopt/befit/data/MySizeLookupData.kt
@@ -0,0 +1,8 @@
+package com.sopt.befit.data
+
+data class MySizeAddData(
+        val b_name: String,
+        val p_name: String,
+        val p_price: String,
+        var p_like : Boolean
+)
