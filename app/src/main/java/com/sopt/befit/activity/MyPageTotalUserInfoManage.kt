74cadc2e8b7bd189a4ef70c6e6f2371212a78491
Jeongmin <han51361@naver.com>
2019-01-03 14:12:21 +0900

    add may_page_total_user_info_manage activity

---
+++ app/src/main/java/com/sopt/befit/activity/MyPageTotalUserInfoManage.kt
@@ -0,0 +1,13 @@
+package com.sopt.befit.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sopt.befit.R
+
+class MyPageTotalUserInfoManage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page_total_user_info_manage)
+    }
+}
