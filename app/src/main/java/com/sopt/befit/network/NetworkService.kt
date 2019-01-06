cbe876a2dcf4f5aaac3bbbc0a5ec89ea4f66a168
jegaleun <jegaleun0323@gmail.com>
2019-01-06 19:59:17 +0900

    브랜드이니셜통신

--- app/src/main/java/com/sopt/befit/network/NetworkService.kt
+++ app/src/main/java/com/sopt/befit/network/NetworkService.kt
@@ -2,16 +2,15 @@ package com.sopt.befit.network
 
 import com.google.gson.JsonObject
 import com.sopt.befit.data.*
+import com.sopt.befit.get.GetInitialBrandResponse
+import com.sopt.befit.get.GetInitialGoodsResponse
 import com.sopt.befit.post.PostForPwFindUserResponse
 import com.sopt.befit.post.PostLoginResponse
 import com.sopt.befit.post.PostSignUpResponse
 import com.sopt.befit.post.PostTotalUserDataResponse
 import com.sopt.befit.put.PutModifyPwResponse
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.POST
-import retrofit2.http.PUT
+import retrofit2.http.*
 
 interface NetworkService {
 
@@ -51,4 +50,17 @@ interface NetworkService {
         @Header ("Content-Type: application/json")
         @Body combineFormData: CombineFormData
     ): Call<PostTotalUserDataResponse>
+
+    @GET("/brands")
+    fun getBrandsByInitialResponse(
+            @Header("Authorization") authorization: String,
+            @Query("initial") initial : Char
+    ) : Call<GetInitialBrandResponse>
+
+    @GET("/closet/brands/{brand_idx}/category/{category_idx}")
+    fun getGoodsByInitialResponse(
+            @Header("Authorization") authorization: String,
+            @Path("brand_idx") brand_idx : Int,
+            @Path("category_idx") category_idx : Int
+    ) : Call<GetInitialGoodsResponse>
 }
