47cbea21cfb77b7ac5b735efa76bb12b76f7e5bb
Jeongmin <han51361@naver.com>
2019-01-06 21:11:55 +0900

    js file

--- app/src/main/java/com/sopt/befit/activity/KakaoAddressActivity.kt
+++ app/src/main/java/com/sopt/befit/activity/KakaoAddressActivity.kt
@@ -12,10 +12,11 @@ import com.sopt.befit.R
 class KakaoAddressActivity : AppCompatActivity() {
 
 
-    lateinit var webView : WebView
-    lateinit var result : TextView
-    lateinit var  handler : Handler
+    lateinit var webView: WebView
+    lateinit var result: TextView
+    lateinit var handler: Handler
 
+    var url: String = "file:///android_asset/daum.js"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kakao_address)
@@ -26,30 +27,27 @@ class KakaoAddressActivity : AppCompatActivity() {
     }
 
 
-    public fun init_webView(){
+    fun init_webView() {
         //web view 설정
         webView = findViewById(R.id.wv_activity_kakao_address)
 
         //javascript 허용
         webView.settings.javaScriptEnabled = true
         //javascript window open 허용
-        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true)
         webView.settings.javaScriptCanOpenWindowsAutomatically = true
         //javascript이벤트에 대응할 함수를 정의 한 클래스를 붙여줌
         //두번째 param은 사용될 php에도 동일하게 사용해줘야한다.
-        webView.addJavascriptInterface(AndroidBridge(),"TestApp")
+        webView.addJavascriptInterface(AndroidBridge(), "TestApp")
         webView.webChromeClient = WebChromeClient()
         // webview url load
-        webView.loadUrl("http://codeman77.ivyro.net/getAddress.php")
-
-
+        webView.loadUrl(url)
 
 
     }
 
-    inner class AndroidBridge{
+    inner class AndroidBridge {
         @JavascriptInterface
-        public fun setAddress(arg1 : String,arg2 : String, arg3 : String ){
+        fun setAddress(arg1: String, arg2: String, arg3: String) {
             handler.post {
                 result.text = String.format("(%s) %s %s", arg1, arg2, arg3)
                 // WebView를 초기화 하지않으면 재사용할 수 없음
