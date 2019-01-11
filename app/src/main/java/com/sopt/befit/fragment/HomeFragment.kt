5052d57c2d05e5454c84d70a7a442af34b072d2f
송지현 <sgh280@naver.com>
2019-01-12 00:05:21 +0900

    no message

--- app/src/main/java/com/sopt/befit/fragment/HomeFragment.kt
+++ app/src/main/java/com/sopt/befit/fragment/HomeFragment.kt
@@ -281,9 +281,9 @@ class HomeFragment : Fragment() {
 
     private fun configureBannerNavigation() {
 
-//        vp_aaa_main_banner.adapter = HomeFragmentBannerPagerAdapter(childFragmentManager, 3)
-//        vp_aaa_main_banner.offscreenPageLimit = 3
-//        tabDots.setupWithViewPager(vp_aaa_main_banner)
+        vp_aaa_main_banner.adapter = HomeFragmentBannerPagerAdapter(childFragmentManager, 3)
+        vp_aaa_main_banner.offscreenPageLimit = 3
+        tabDots.setupWithViewPager(vp_aaa_main_banner)
 
     }
 
